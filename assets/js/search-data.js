e26722cc15f4165d2b2e4b2140a2d1f1de05a132
corvus-ch <corvus-ch@users.noreply.github.com>
2022-04-26 18:23:12 +0000

    deploy: f0f9cb9f5095f04c1137e5b4ceefc2fe3ffb9d6a

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "A better way to handle audio devices for digital modes",
+        "excerpt":"Working with digital modes, usually, is done by paring a computer with a transceiver.This mostly involves some sort of audio device.That audio device then is used to pass audio from the transceiver to the software doing the digital stuff, and the other way around.Documentations for various digital modes, explains to...","categories": [],
+        "tags": [],
+        "url": "https://hb9hox.radio/en/2022-04-26-digital-modes-and-audio-devices.html"
+      }]
