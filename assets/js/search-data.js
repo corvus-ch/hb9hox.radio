b7de639471b9ab4b6f26d898a82cd925374ba14a
corvus-ch <corvus-ch@users.noreply.github.com>
2022-04-27 17:00:50 +0000

    deploy: e384a120656540a914fd8754a456b55d7e5c5870

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Working with digital modes, usually, is done by paring a computer with a transceiver.This mostly involves some sort of audio device.That audio device then is used to pass audio from the transceiver to the software doing the digital stuff, and the other way around.Documentations for various digital modes, explains to...","categories": [],
         "tags": [],
         "url": "https://hb9hox.radio/en/2022-04-26-digital-modes-and-audio-devices.html"
+      },{
+        "title": "Vereinfacht Verwendung von Audiogeräte für digitale Modi",
+        "excerpt":"Das Arbeiten mit digitalen Modi erfolgt normalerweise durch Koppeln eines Computers mit einem Transceiver.Dies beinhaltet meistens eine Art Audiogerät, welches dann verwendet wird, um Audio vom Transceiver an die Software weiterzuleiten, die die digitalen Dinge erledigt, und umgekehrt.Dokumentationen zu verschiedenen digitale Modi, erklärt, wie Sie komplizierte Schritte und kryptische Gerätenamen...","categories": [],
+        "tags": [],
+        "url": "https://hb9hox.radio/de/2022-04-27-digitale-modi-und-audiogeraete.html"
       }]
