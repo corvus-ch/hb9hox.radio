7391f14db8c696e461f0d8f4f57788886aae9c1a
Christian Haeusler <794584+corvus-ch@users.noreply.github.com>
2022-10-16 19:59:14 +0200

    Switch to Hugo

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM jekyll/jekyll:4
-
-ADD Gemfile* /srv/jekyll/
-
-RUN bundle install
--- _plugins/locale.rb
+++
@@ -1,6 +0,0 @@
-# Replace the sites locale with the pages language
-Jekyll::Hooks.register [:pages, :documents], :post_convert do |doc|
-  if doc.data.has_key?("lang")
-    doc.site.config["locale"] = doc.data["lang"]
-  end
-end
